df2b7b8611810878e70f10a4f43377a551a7b4dd
Yura <idblablobla@gmail.com>
2021-12-16 02:36:50 +0300

    + DATA BASE

---
+++ Codes.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+
+namespace TPExpCon
+{
+	public static class Codes
+	{
+		/// <summary>
+		/// How much codes codes read each time. Default - 50
+		/// </summary>
+		public static int CodesChunkSize = 10;
+
+		/// <summary>
+		/// Length of one code
+		/// </summary>
+		private static int CodeLength { get; } = 33;
+		/// <summary>
+		/// Get IEnumerable with 50 readed from file codes
+		/// </summary>
+		/// <param name="codesFile">File with codes to read</param>
+		/// <returns></returns>
+		public static IEnumerable<string> GetCodesFromFile(string codesFile)
+		{
+			var lines = new List<string>();
+			try
+			{
+				Console.WriteLine("Сколько кодов взять из файла?");
+				String buffer = Console.ReadLine();
+				try
+				{
+					if (Int32.TryParse(buffer, out CodesChunkSize));
+					else throw new Exception("Некорректные данные");
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e.Message);
+				}
+				using (var codesFileStreamReader = new StreamReader(codesFile))
+				{
+					for (int i = 0; i < CodesChunkSize; i++)
+					{
+						var code = codesFileStreamReader.ReadLine();
+						var modifiedCode = ReplaceGsSymbols(code);
+						lines.Add(modifiedCode);
+					}
+				}
+			}
+			catch (FileNotFoundException)
+			{
+				throw new FileNotFoundException("No such file with codes");
+			}
+			return lines;
+		}
+		
+		
+		public static IEnumerable<string> GetCodesFromDatabase()
+		{
+			var lines = new List<string>();
+			Console.WriteLine("Сколько кодов взять из базы данных?");
+			String buffer = Console.ReadLine();
+			try
+			{
+				if (Int32.TryParse(buffer, out CodesChunkSize));
+				else throw new Exception("Некорректные данные");
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+			return new DatabaseHandler().getCodes();
+		}
+		/// <summary>
+		/// Return one code on the <param name="number"/> position
+		/// </summary>
+		/// <param name="codesFile">File with codes to read</param>
+		/// <param name="number">Number of code</param>
+		/// <returns></returns>
+		public static string GetOneCode(string codesFile, int number=0)
+		{
+			string line;
+			try
+			{
+				using (var codesStreamReader = new StreamReader(codesFile))
+				{
+
+					//_codesStream ??= new FileStream(codesFile, FileMode.Open);
+					//var offset = number * CodeLength;
+					//_codesStream.Seek(offset, SeekOrigin.Begin);
+					for (int i = 1; i < number; i++) codesStreamReader.ReadLine();
+					var code = codesStreamReader.ReadLine();
+					var modifiedCode = ReplaceGsSymbols(code);
+					line = modifiedCode;
+				}
+			}
+			catch
+			{
+				throw;
+			}
+			return line;
+		}
+		/// <summary>
+		/// Replace GS1 symbol by (char)29 and append code by it
+		/// </summary>
+		/// <param name="code">code to replace symbols</param>
+		public static string ReplaceGsSymbols(string code) => $"{(char)29}{code.Replace('', (char)29)}";
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System;
+
+namespace TPExpCon
+{
+	public class Program
+	{
+		static void Main()
+		{
+			Console.Title = " ";
+			Console.BackgroundColor = ConsoleColor.White;
+			Console.Clear();
+
+			String path;
+			Console.Write("Введите путь к файлу с кодами\n");
+			path = Console.ReadLine();
+			Console.ForegroundColor = ConsoleColor.Black;
+			Console.BackgroundColor = ConsoleColor.White;
+			var codes = Codes.GetCodesFromDatabase();
+			List<string> list = codes.ToList();
+			
+			Printer.PrintOneByOne(list, new Point(90, 150), new Point(180, 180));
+		}
+	}
+}
